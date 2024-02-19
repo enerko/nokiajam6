4aa115ad1564818854918ab2977375aa11f8c5ed
Andy Wang <andy_wang02@hotmail.com>
2024-02-19 18:09:57 -0500

    hopefully fixed getting stuck on corners

---
+++ Assets/Scripts/SnapToUnitOnCollide.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Snap object's position to whole numbers
+public class SnapToUnitOnCollide : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D _collision)
+    {
+        Debug.Log("colided");
+        _rb.position = Vector2Int.RoundToInt(_rb.position);
+    }
+}
